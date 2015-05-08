8c98841856901398e81182e02a4b4643e2069afe
Antonin Décimo <antonin.decimo@gmail.com>
2015-05-08 18:38:49 +0200

    Show thumbnails for upvoted and commented

--- view/pages/user.php
+++ view/pages/user.php
@@ -49,27 +49,25 @@ foreach ($authored as $f) {
 
     <section id="upvoted-section">
         <h2>Upvoted</h2>
+            <table>
 <?php
+$i = 0;
 foreach ($upvoted as $f) {
+    if ($i % 5 == 0)
+        echo "<tr>\n";
 ?>
-    <article>
-        <h3>
-            <span class="title"><?php echo $f->title(); ?></span> by
-            <a class="author" href="/user?id=<?php echo $u->id(); ?>"><?php echo $u->name(); ?></a> on
-            <span class="date"><?php echo $f->date("d/m/y H:i"); ?></span>
-        </h3>
-        <a href="/fractal?id=<?php echo $f->id(); ?>"><canvas width="800" height="600" data-formula="<?php echo htmlentities($f->formula()); ?>"></canvas></a>
-        <p class="votes">Votes : <span class="vote" data-id="<?php echo $f->id();?>"><?php echo $f->votes(); ?></span></p>
-<?php if ($um->hasLoggedInUser()) { ?>
-        <button<?php if ($um->loggedUser()->hasUpvoted($f)) echo ' class="upvoted"'; ?> data-id="<?php echo $f->id(); ?>" data-role="upvote">Upvote</button>
-        <button<?php if ($um->loggedUser()->hasDownvoted($f)) echo ' class="downvoted"'; ?> data-id="<?php echo $f->id(); ?>" data-role="downvote">Downvote</button>
-<?php } else { ?>
-        <form action="/connect"><input type="submit" value="Upvote"><input type="submit" value="Downvote"></form>
-<?php } ?>
-    </article>
+                <td><a href="/fractal?id=<?php echo $f->id(); ?>">
+                    <canvas width="200" height="200" title="<?php echo htmlentities($f->title()); ?>" data-formula="<?php echo htmlentities($f->formula()); ?>"></canvas>
+                </a></td>
 <?php
+    $i++;
+    if ($i % 5 == 0)
+        echo "</tr>\n";
 }
+if ($i - 1 % 5 != 0)
+    echo "</tr>\n";
 ?>
+        </table>
     </section>
 
 <?php } if (count($commented) > 0) { ?>
@@ -77,27 +75,25 @@ foreach ($upvoted as $f) {
     <section id="commented-section">
     <h2>Commented</h2>
 <?php
+$i = 0;
 foreach ($commented as $f) {
+    if ($i % 5 == 0)
+        echo "<tr>\n";
 ?>
-    <article>
-        <h3>
-            <span class="title"><?php echo $f->title(); ?></span> by
-            <a class="author" href="/user?id=<?php echo $u->id(); ?>"><?php echo $u->name(); ?></a> on
-            <span class="date"><?php echo $f->date("d/m/y H:i"); ?></span>
-        </h3>
-        <a href="/fractal?id=<?php echo $f->id(); ?>"><canvas width="800" height="600" data-formula="<?php echo htmlentities($f->formula()); ?>"></canvas></a>
-        <p class="votes">Votes : <span class="vote" data-id="<?php echo $f->id();?>"><?php echo $f->votes(); ?></span></p>
-<?php if ($um->hasLoggedInUser()) { ?>
-        <button<?php if ($um->loggedUser()->hasUpvoted($f)) echo ' class="upvoted"'; ?> data-id="<?php echo $f->id(); ?>" data-role="upvote">Upvote</button>
-        <button<?php if ($um->loggedUser()->hasDownvoted($f)) echo ' class="downvoted"'; ?> data-id="<?php echo $f->id(); ?>" data-role="downvote">Downvote</button>
-<?php } else { ?>
-        <form action="/connect"><input type="submit" value="Upvote"><input type="submit" value="Downvote"></form>
-<?php } ?>
-    </article>
+                <td><a href="/fractal?id=<?php echo $f->id(); ?>">
+                    <canvas width="200" height="200" title="<?php echo htmlentities($f->title()); ?>" data-formula="<?php echo htmlentities($f->formula()); ?>"></canvas>
+                </a></td>
 <?php
+    $i++;
+    if ($i % 5 == 0)
+        echo "</tr>\n";
 }
+if ($i - 1 % 5 != 0)
+    echo "</tr>\n";
 ?>
+        </table>
     </section>
+
 <?php } ?>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
